16a861b2e417642a46cbcf25374f89a6280c0dc8
Anurag Pal <apal895@gmail.com>
2021-11-08 22:07:38 +0530

    separate endpoitns

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ var express = require("express");
 var cors = require("cors");
 var app = express();
 const axios = require("axios");
-var FormData = require("form-data");
-var fs = require("fs");
-var { Blob } = require("buffer");
 
 const PORT = process.env.PORT || 5000;
 
@@ -20,42 +17,23 @@ app.get("/", function (req, res, next) {
   });
 });
 
-const dataURLToFile = async (dataurl, name) => {
-  var regex = /^data:.+\/(.+);base64,(.*)$/;
-  var matches = dataurl.match(regex);
-  var ext = matches[1];
-  var data = matches[2];
-  var buffer = Buffer.from(data, "base64");
-  await fs.writeFileSync(name + "." + ext, buffer);
-  return ext;
-};
-
-function DataURIToBlob(dataURI) {
-  const splitDataURI = dataURI.split(",");
-  const byteString =
-    splitDataURI[0].indexOf("base64") >= 0
-      ? atob(splitDataURI[1])
-      : decodeURI(splitDataURI[1]);
-  const mimeString = splitDataURI[0].split(":")[1].split(";")[0];
-
-  const ia = new Uint8Array(byteString.length);
-  for (let i = 0; i < byteString.length; i++) ia[i] = byteString.charCodeAt(i);
-
-  return new Blob([ia], { type: mimeString });
+async function getResultForm(b64File) {
+  const headers = {
+    "Content-Type": "application/json",
+  };
+  return await axios.post(
+    "http://164.52.218.27:8000/wfpredict/ocr",
+    b64File.split(",")[1],
+    {
+      headers,
+    }
+  );
 }
 
-async function getResult(b64File) {
+async function getResultReceipt(b64File) {
   const headers = {
-    // Accept: "application/json",
     "Content-Type": "application/json",
-    // "Content-Security-Policy": "upgrade-insecure-requests",
   };
-  // const formData = new FormData();
-  // const file = DataURIToBlob(b64File, "temp-image");
-  // //   console.log(file);
-  // // const file = fs.createReadStream("/temp-image." + ext);
-  // console.log(file);
-  // formData.append("data", file);
   return await axios.post(
     "http://164.52.218.27:7080/wfpredict/ocr",
     b64File.split(",")[1],
@@ -65,15 +43,22 @@ async function getResult(b64File) {
   );
 }
 
-app.post("/predict", async (req, res) => {
+app.post("/predictForm", async (req, res) => {
   const { data } = req.body;
-  // console.log(req.body);
-  // if (data) {
-  // console.log("Found");
-  const result = await getResult(data);
+  if (data) {
+    const result = await getResultForm(data);
 
-  res.status(200).json(result.data);
-  // }
+    res.status(200).json(result.data);
+  }
+});
+
+app.post("/predictReceipt", async (req, res) => {
+  const { data } = req.body;
+  if (data) {
+    const result = await getResultReceipt(data);
+
+    res.status(200).json(result.data);
+  }
 });
 
 app.listen(PORT, function () {
